421eb2bb535868f627dea3924e57b65dbf8feaee
Jeremias Ceciliano <jecel0911@gmail.com>
2016-11-29 13:42:05 -0600

    adding constants class

---
+++ app/src/main/java/com/helloandroid/jceciliano/spotifyapp/common/Constants.java
@@ -0,0 +1,13 @@
+package com.helloandroid.jceciliano.spotifyapp.common;
+
+/**
+ * Created by jceciliano on 29/11/16.
+ *
+ * Global constants
+ */
+
+public final class Constants {
+    public static final String SPOTIFY_API = "https://api.spotify.com";
+    public static final String ARTIST_SEARCH = "/v1/search?type=artist";
+    public static final String QUERY_SEARCH = "q";
+}
